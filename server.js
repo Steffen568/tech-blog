3814dc8303919eb7ced73b67e339e6252cc78aeb
Steffen568 <sgonzalez568@gmail.com>
2022-06-21 11:59:58 -0500

    comment/post/vote/user models created. associations linked in index.js

--- server.js
+++ server.js
@@ -23,14 +23,14 @@ app.use(session(sess));
 
 const hbs = exphbs.create({});
 
-// app.engine('handlebars', hbs.engine);
-// app.set('view engine', 'handlebars');
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-// app.use(require('./controllers/'));
+app.use(require('./controllers/'));
 
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log('Now listening'));
